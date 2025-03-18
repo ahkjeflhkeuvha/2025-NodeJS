0fe42d394fcbea270e413d52a3b55d357259b430
오지은 <w2317@e-mirim.hs.kr>
2025-03-18 15:49:18 +0900

    express 설치

---
+++ for.js
@@ -0,0 +1,9 @@
+const fruits = ["banana", "apple", "grape"]
+
+for(let f of fruits) {
+    console.log(f)
+}
+
+fruits.forEach((val) => {
+    console.log(val)
+})
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-function 문자출력함수(문자){
-  console.log('문자 출력 함수' + 문자);
-};
+// function 문자출력함수(문자){
+  // console.log('문자 출력 함수' + 문자);
+// };
 
-문자출력함수('a')
 
 익명함수 = function(문자){
   console.log("익명함수 " + 문자);
 }
 
 익명함수("swag")
+
+let 문자출력함수 = (문자) => console.log('문자출력함수 ' + 문자)
+
+문자출력함수('a')
