9cfddcf93fa1a2a19bc61248505cc8e166c04e22
오지은 <w2317@e-mirim.hs.kr>
2025-04-01 15:04:07 +0900

    Read 완성

--- app2.js
+++ app2.js
@@ -2,50 +2,64 @@ const express = require("express");
 const path = require("path");
 const swagRoute = require("./Routes/swagRoute");
 const mysql2 = require("mysql2");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
+
+console.log(__dirname);
 
 const db = mysql2.createConnection({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
+  if (err) {
+    console.log("fail : " + err);
+  }
+  console.log("success");
+});
+
+app.get("/", (req, res) => {});
+
+app.get("/travel", (req, res) => {
+  let travelList;
+  const query = "SELECT id, name FROM travelList";
+  db.query(query, (err, results) => {
     if (err) {
-        console.log("fail : " + err);
+      res.status(500).json({ message: "error" });
+      return;
     }
-    console.log("success")
+    travelList = results;
+    console.log(travelList);
+    res.render("travel", { travelList });
+  });
+  // res.status(200).json({ message : 'success'});
 });
 
-app.get('/', (req, res) => {
-    
+app.get("/travel/:id", (req, res) => {
+  const travelId = req.params.id;
+  const _query = "SELECT * FROM travelList WHERE id = ?";
+  db.query(_query, [travelId], (err, results) => {
+    if (err) {
+      res.status(500).send("Internal SErver Error");
+    }
+    if (results.length == 0) {
+      res.status(404).send("No Data");
+    }
+    const travel = results[0];
+    res.render("travelDetail", { travel });
+  });
 });
 
-app.get('/travel', (req, res) => {
-    let travelList;
-    const query = 'SELECT id, name FROM travelList';
-    db.query(query, (err, results) => {
-        if(err) {
-            res.status(500).json({ message : "error"});
-            return;
-        }
-        travelList = results;
-        console.log(travelList);
-        res.render('travel', {travelList});
-    })
-    // res.status(200).json({ message : 'success'});
-    
-})
-
-app.use('/swag',  swagRoute);
+app.use("/swag", swagRoute);
 
 app.listen(3000, () => {
-    console.log(__dirname); // __dirname은 현재 디렉토리에 해당하는 절대경로 - path.join(__dirname, 'views')를 사용하면 경로 지정자에 상관없이 폴더를 만들어줌
-    console.log("conntected!")
-})
+  console.log(__dirname); // __dirname은 현재 디렉토리에 해당하는 절대경로 - path.join(__dirname, 'views')를 사용하면 경로 지정자에 상관없이 폴더를 만들어줌
+  console.log("conntected!");
+});
