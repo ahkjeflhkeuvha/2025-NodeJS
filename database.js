216c5a7d668fcb4669813f134e527ef03cac58b9
오지은 <w2317@e-mirim.hs.kr>
2025-04-15 14:49:56 +0900

    createPool 예제

--- database.js
+++ database.js
@@ -2,19 +2,11 @@ const mysql2 = require("mysql2");
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql2.createConnection({
+const db = mysql2.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
-
-db.connect((error) => {
-  if (error) {
-    console.error("Database connection failed:", error);
-    throw error;
-  }
-  console.log("db connected!");
 });
 
 module.exports = db;
